cd131bba93dfb5481d614f0edc62aa613840ef9a
Brandon Rowe <brandonhrowe@gmail.com>
2019-07-01 21:53:43 -0400

    first commit

---
+++ ffmpeg-test.py
@@ -0,0 +1,42 @@
+import ffmpeg
+import random
+import ffmpy
+import subprocess
+
+# in1 = ffmpeg.input('https://archive.org/download/Detour_movie/Detour.mp4', ss=60, t=90)
+# in2 = ffmpeg.input('https://archive.org/download/Detour_movie/Detour.mp4', ss=240, t=90)
+# v1 = in1.video.hflip()
+# a1 = in1.audio
+# v2 = in2.video.filter('reverse').filter('hue', s=0)
+# a2 = in2.audio.filter('areverse').filter('aphaser')
+# joined = ffmpeg.concat(v1, a1, v2, a2, v=1, a=1).node
+# v3 = joined[0]
+# a3 = joined[1].filter('volume', 0.8)
+# out = ffmpeg.output(v3, a3, 'out.mp4')
+# out.run()
+
+# main = ffmpeg.input('https://archive.org/download/Detour_movie/Detour.mp4', t=90)
+# main = ffmpeg.input('WoTClip.mov')
+# logo = ffmpeg.input('MMofaStreet.jpg')
+
+# (
+#     ffmpeg
+#     .filter([main, logo], 'overlay', 500, 500)
+#     .output('WoT_Output_Burnin.mp4')
+#     .run()
+# )
+
+# probe = ffmpeg.probe('https://archive.org/download/Detour_movie/Detour.mp4')
+# print(probe)
+
+ff = ffmpy.FFmpeg(
+  inputs={"https://archive.org/download/Detour_movie/Detour.mp4": '-hide_banner'},
+  outputs={"pipe:1": '-an -filter:v "select=\'gt(scene, 0.4)\', showinfo" -f null'}
+)
+ff.cmd
+stdout, stderr = ff.run(stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+# print("stdout:", stdout)
+# print("stderr:", stderr)
+# output = filter(lambda x : "[Parsed_showinfo" in x, stderr.decode("utf-8").split("\n"))
+output = stderr.decode("utf-8").split("\n")
+print(output)
